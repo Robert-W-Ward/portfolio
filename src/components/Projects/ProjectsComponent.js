1de674a5afdd91d74e75f7f691ea4ac9f442518d
Robert Ward <robertwesleyward2019@gmail.com>
2023-06-08 13:49:11 -0500

    update projects section to have project card for portfolio

--- src/components/Projects/ProjectsComponent.js
+++ src/components/Projects/ProjectsComponent.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./ProjectsComponentStyles.scss";
 import ProjectCard from "../../components/ProjectCard/ProjectCardComponent.js";
-import DevilImage from '../../assets/images/devilf.png'
+import PortfolioLogoImage from '../../assets/images/robert_ina_box.png'
+import TestImage from '../../assets/images/test.png'
 import Carousel from "../../components/Carousel/CarouselComponent.js";
 
 
@@ -9,26 +10,26 @@ import Carousel from "../../components/Carousel/CarouselComponent.js";
 const Project = () => {
   const slides = [
     <ProjectCard
-      title="Project 1"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-      imageUrl={DevilImage}
+      title="This very Website!"
+      description="This website was made me to showcase the projects I'm most proud of. It is a single page web app written using React.js"
+      imageUrl={PortfolioLogoImage}
     />,
-    <ProjectCard
-      title="Project 2"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-      imageUrl=""
-    />,
-    <ProjectCard
-      title="Project 3"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-      imageUrl='https://placehold.co/512x512'
-    />
+    // <ProjectCard
+    //   title="Project 2"
+    //   description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    //   imageUrl={TestImage}
+    // />,
+    // <ProjectCard
+    //   title="Project 3"
+    //   description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    //   imageUrl='https://placehold.co/512x512'
+    // />
   ]
   return (
     <section className="projects-section" id="projects">
       <div className="projects-container">
         <h2 className="section-title">Projects</h2>
-        <Carousel slides={slides}/>
+        <Carousel slides={slides} />
       </div>
     </section>
   );
